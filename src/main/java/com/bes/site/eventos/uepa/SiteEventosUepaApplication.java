f61bfc72599d07d370d8290b7de2d414b1f435fc
Amom Mirando <mirandoamom@gmail.com>
2025-12-10 18:44:18 -0300

    Backend Java Spring + Page Home e Page Eventos(incompleta)

---
+++ src/main/java/com/bes/site/eventos/uepa/SiteEventosUepaApplication.java
@@ -0,0 +1,13 @@
+package com.bes.site.eventos.uepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEventosUepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEventosUepaApplication.class, args);
+	}
+
+}
